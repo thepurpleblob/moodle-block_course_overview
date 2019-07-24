66732c266358783fdf46f04687942e6c8b83d434
Yann Cuttaz <cuttay@usi.ch>
2019-07-24 09:46:09 +0200

    version 3.6.0

--- version.php
+++ version.php
@@ -28,4 +28,4 @@ $plugin->component = 'block_course_overview';
 $plugin->version =  2019072400;
 $plugin->requires = 2017051500;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.5.0';
+$plugin->release = '3.6.0';
